26f8b24b5b17fa7986b09d82905dd1f7fd330acc
Sophia <scohenpelletier1@hwemail.com>
2026-02-09 13:55:48 -0800

    1.1.4: Added Wall Collisions
    Added wall collisions for the player so that it can only move in a certain area. This makes me remember why I like unity more than godot, it can handle collisions so much better. I'm suffering for you Aylin :(

--- Scripts/playerBehavior.gd
+++ Scripts/playerBehavior.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
-@export var speed = 3
+var speed := Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# starting position for the player
 	global_position.x = 960
 	global_position.y = 91
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	global_position.x = get_global_mouse_position().x
+func _physics_process(delta: float) -> void:
+	var target_x = get_global_mouse_position().x
+	velocity = Vector2((target_x - global_position.x) / delta, 0)
+	move_and_slide()
